f1028dedcfba62e45ccc704be391500e271babaf
Eric Belongea <belongeae@infinity-software.com>
2024-05-06 07:16:47 -0600

    Profile Icons with CVAs

--- src/app/profile-icon/profile-icon.component.ts
+++ src/app/profile-icon/profile-icon.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Provider, forwardRef } from '@angular/core';
 import { profileIconNames } from './profile-icon-names';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const PROFILE_ICON_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => ProfileIconComponent),
+  multi: true,
+}
 
 @Component({
   selector: 'con-profile-icon',
   templateUrl: './profile-icon.component.html',
-  styleUrls: ['./profile-icon.component.css']
+  styleUrls: ['./profile-icon.component.css'],
+  providers: [PROFILE_ICON_VALUE_ACCESSOR],
 })
-export class ProfileIconComponent {
+export class ProfileIconComponent implements ControlValueAccessor{
   profileIcons = profileIconNames;
   showAllIcons: boolean = true;
   selectedIcon!: string | null
 
+  private onChange!: Function;
+  private onTouched!: Function;
+
+  writeValue(icon: string | null) {
+    this.selectedIcon = icon;
+    if (icon && icon !== '')
+      this.showAllIcons = false;
+    else
+    this.showAllIcons = true;
+  }
+
+  registerOnChange(fn: Function){
+    this.onChange = (icon: string) => { fn(icon); };
+  }
+
+  registerOnTouched(fn: Function){
+    this.onTouched = fn;
+  }
+
   iconSelected(icon: string) {
     this.showAllIcons = false;
     this.selectedIcon = icon;
+    this.onChange(icon);
   }
 }
